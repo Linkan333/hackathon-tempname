24009c3875a1d0de2bd56815e83251262ce37184
Linkan333 <linunybe@gmail.com>
2026-01-28 16:41:20 +0100

    Initial

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const path = require('path');
+const fs = require('fs');
 const port = 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
+
+app.use(session({
+  secret: 'hackathon-temp-token',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 * 3000 }
+}));
 
 const difficulties = new Map();
 
@@ -13,20 +23,36 @@ difficulties.set("Medium", 2);
 difficulties.set("Hard", 3);
 
 
-class Level {
+class Player {
+  constructor(username) {
+    this.username = username;
+    this.experience = 0;
+    this.currentLevel = 1;
+    this.sessionId = null;
+  }
+  streakBegun() {
+    let streak;
+    streak += 1;
+  };
+
+  addXp(amount) {
+    this.experience += amount;
+    console.log(`${this.username} now has ${this.experience} XP`);
+  }
+}
+
+/*class Level {
   constructor(username, experience, currentLevel) {
     this.username = username;
     this.experience = experience;
     this.currentLevel = currentLevel;
   }
   addXp(xp) {
-    if () {
       this.experience;
       this.experience += xp;
       console.log(`${this.username} now has ${this.experience} XP`);
-    }
   }
-}
+}*/
 
 
 class Challenges {
@@ -35,23 +61,38 @@ class Challenges {
     this.challengeDifficulty = challengeDifficulty;
     this.challengeAnswer = challengeAnswer;
   }
-  validateAnswer(userAnswer) {
+
+  /*dayStreak(player) {
+
+  }*/
+
+  validateAnswer(userAnswer, player) {
     if (this.challengeAnswer.includes(userAnswer)) {
-      let levels = [difficulties.values()];
-      switch (levels) {
+      const xp = difficulties.get(this.challengeDifficulty);
+      let reward = 0;
+
+      switch (xp) {
         case 1:
-          Level.addXp(25); 
+          reward = 25;
           break;
         case 2:
-          Level.addXp(50);
+          reward = 60;
           break;
-        case 3:
-          Level.addXp(100);
+        case 3: 
+          reward = 100;
           break;
       }
-    //return this.challengeAnswer.includes(userAnswer);
+
+
+      player.addXp(reward);
+      return true;
+      //streakBegun(player);
+
+    }
+    return false;
   }
 }
+
 
 //const level1 = new Level("Daniel", 0);
 
@@ -64,24 +105,86 @@ class Challenges {
 //console.log(challenge1.validateAnswer("43"));
 
 //const challenge2 = new Challenges("Vilken färg är gräs", "Easy", ["42", "forty-two", "Forty Two"]);
-
-
 //const result = challenge2.validateAnswer("42");
 //console.log("Challenge result: ", result);
 //level1.addXp(result);
 
 
 
+const players = new Map();
+var challenges = [];
+
+
+
+
+app.post('/create-challenge', (req, res) => {
+  const { challengeName, challengeDifficulty, challengeAnswer } = req.body;
+
+  if (!challengeName || !challengeDifficulty || !challengeAnswer) {
+    return res.status(400).send("Missing fields");
+  }
+
+
+  const answersArray = challengeAnswer.split(",").map(a => a.trim());
+
+
+  const challenge = new Challenges(
+    challengeName,
+    challengeDifficulty,
+    answersArray
+  );
+
+  challenges.push(challenge);
+
+  fs.writeFile("challenges.json", JSON.stringify(challenges, null, 2), (err) => {
+    if (err) {
+      console.error("Error saving challenges: ", err);
+    } else {
+      console.log("Challenge saved to challenges.json")
+    }
+  });
+
+  res.send({
+    message: "Challenge created",
+    challenge
+  });
+});
+
+
+app.get('/create-challenge', (req, res) => {
+
+  res.sendFile(__dirname + '/public/create.html');
+});
+
+app.get('/challenges', (req, res) => {
+  res.sendFile(__dirname + '/public/challenges.html');
+});
+
 
 app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/public/login/login.html');
-});
+  res.sendFile(__dirname + "/public/login/login.html");
+})
 
 app.post('/login', (req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  res.send(`Username: ${username} & Password: ${password}`);
+  const { username } = req.body;
+
+  if (!username) return res.send("Username required");
+
+
+  if (!req.session.player) {
+    const player = new Player(username);
+    player.sessionId = req.sessionID;
+    req.session.player = player;
+  }
+
+  res.send({
+    message: "Player logged in",
+    player: req.session.player
+  });
 });
+
+
+
 
 
 app.listen(port, () => {
