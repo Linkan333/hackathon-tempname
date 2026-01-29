3d7f7226f4efbea8b75aefc4ad0656aad2c244e3
Kristoffer <kristoffer.wahlback@elev.ga.lbs.se>
2026-01-29 09:49:55 +0100

    Ny Clean Design (EdTech-stil) + Fungerande Quiz Modal med JS

---
+++ script.js
@@ -0,0 +1,58 @@
+const startButtons = document.querySelectorAll('.btn-start-task');
+const modal = document.getElementById('quiz-modal')
+const closeBtn = document.getElementById('close-btn')
+
+const flipBtn = document.querySelector('.btn-flip');
+const quizCard = document.querySelector('.quiz-card');
+const cardFront = document.querySelector('.card-front');
+const cardBack = document.querySelector('.card-back');
+
+const failBtn = document.querySelector('.btn-fail');
+const btnSuccess = document.querySelector('.btn-success');
+
+const xpDisplay = document.getElementById('score');
+let currentXP = 0;
+
+startButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+        cardBack.classList.add('hidden');
+    });
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.add('hidden');
+});
+
+function flipCard() {
+    if(!cardFront.classList.contains('hidden')) {
+        cardFront.classList.add('hidden');
+        cardBack.classList.remove('hidden');
+
+    } else {
+        cardFront.classList.remove('hidden');
+        cardBack.classList.add('hidden');
+    }
+}
+
+flipBtn.addEventListener('click', flipCard);
+quizCard.addEventListener('click', flipCard);
+
+successBtn.addEventListener('click', () => {
+    currentXP += 10;
+    xpDisplay.innterText = currentXP;
+
+    modal.classList.add('hidden');
+    alert("Bra jobbat! +10 XP");
+});
+
+failBtn.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    alert("Ingen fara, vi tar det nästa gång!");
+});
+
+modal.addEventListener('click', (e) => {
+    if(e.target === modal) {
+        modal.classList.add('hidden');
+    }
+})
